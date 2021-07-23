bc73190de5016b3d48292fdfa47cfa6b746cf721
Ana Jacobsen <ana.jacobsen@dextra-sw.com>
2021-07-23 15:59:21 -0300

    initial commit for test vercel

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          <a>Testing deploy vercel</a>
         </h1>
 
         <p className={styles.description}>
